13d1d8c46ebb0bca15096feb8f06831496cef32c
Jubril <abdulsalamjubr3@gmail.com>
2025-03-19 16:31:09 +0100

    contact page < reponsiveness

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar"
 import Map from "../components/Map"
+import Contact from "../components/Contact"
+import Footer from "../components/Footer"
 
 const page = () => {
   return (
     <>
         <Navbar />
         <Map />
+        <Contact />
+        <Footer />
     </>
   )
 }
