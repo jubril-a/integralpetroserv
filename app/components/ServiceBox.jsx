788e06fe7f51adfc97c1cc38880ec2437ab2780d
Jubril <abdulsalamjubr3@gmail.com>
2025-04-11 12:53:46 +0100

    implement "service card" close button

--- app/components/ServiceBox.jsx
+++ app/components/ServiceBox.jsx
@@ -1,12 +1,19 @@
 import Image from "next/image"
 import styles from "./serviceCard.module.scss"
+import ActiveContext from "./ActiveContext"
+import { useContext } from "react"
 
 const ServiceBox = ({heading, services, image, imageDesc}) => {
+  let { setActive } = useContext(ActiveContext)
+
+  function clickHandler() {
+    setActive(null)
+  }
 
   return (
     <div className={styles.expanded}>
         <div className={styles.serviceBox}>
-            <Image src="/images/close.png" width="26" height="26" alt="menu" onClick={() => alert("hello")} style={{marginLeft: 'auto', marginBottom: '2rem', display: 'block'}}/>
+            <Image src="/images/close.png" width="26" height="26" alt="menu" onClick={clickHandler} style={{marginLeft: 'auto', marginBottom: '2rem', display: 'block'}}/>
             <h3 className={styles.h3}>{heading}</h3>
             <Image className={styles.img} src={image} alt={imageDesc} width={414} height={232.88}></Image>
             <div>
